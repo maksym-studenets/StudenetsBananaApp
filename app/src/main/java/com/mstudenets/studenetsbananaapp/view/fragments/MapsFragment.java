e49d1eb768dd2fbbe68da9ff12f141a5f277aeb9
Maksym <maksym.studenets@gmail.com>
2017-02-28 22:39:57 +0200

    Added working Google Map with Camera zoom

--- app/src/main/java/com/mstudenets/studenetsbananaapp/view/fragments/MapsFragment.java
+++ app/src/main/java/com/mstudenets/studenetsbananaapp/view/fragments/MapsFragment.java
@@ -1,52 +1,158 @@
 package com.mstudenets.studenetsbananaapp.view.fragments;
 
 
-import android.app.Fragment;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.mstudenets.studenetsbananaapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class MapsFragment extends SupportMapFragment implements
-        OnMapReadyCallback
+
+public class MapsFragment extends Fragment implements
+        OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener
 
 {
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
-    private boolean permissionDenied = false;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 10;
+    private boolean isLocationPermissionGranted;
     private MapView mapView;
     private GoogleMap mMap;
 
     public MapsFragment() {
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.fragment_maps, container, false);
+        View view = inflater.inflate(R.layout.fragment_maps, container, false);
+
         mapView = (MapView) view.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+
+        try {
+            MapsInitializer.initialize(getContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getContext(), "Maps init error", Toast.LENGTH_SHORT).show();
+        }
         mapView.getMapAsync(this);
+
         return view;
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        LatLng chernivtsi = new LatLng(48.2937878, 25.9172208);
-        mMap.addMarker(new MarkerOptions().position(chernivtsi).title("Chernivtsi"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(chernivtsi));
+    public void onMapReady(GoogleMap map) {
+        mMap = map;
+        enableMapControls();
+
+        LatLng city = new LatLng(48.291, 25.935);
+        mMap.addMarker(new MarkerOptions().position(city).title("Chernivtsi"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(city));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2500, null);
+        mMap.setOnMyLocationButtonClickListener(this);
+        enableMyLocation();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    updateUi(isLocationPermissionGranted);
+                } else {
+                    Toast.makeText(getContext(), "Cannot show your current location",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    private void enableMapControls() {
+        UiSettings uiSettings = mMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
+        uiSettings.setZoomGesturesEnabled(true);
+        uiSettings.setMyLocationButtonEnabled(true);
+        uiSettings.setScrollGesturesEnabled(true);
+        uiSettings.setTiltGesturesEnabled(true);
+        uiSettings.setMapToolbarEnabled(true);
+    }
+
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                Toast.makeText(getContext(), "We need location permission to personalize the map",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        } else if (mMap != null) {
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    private void updateUi(boolean isLocationPermissionGranted) {
+        try {
+            if (isLocationPermissionGranted) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                mMap.setMyLocationEnabled(false);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+            }
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
     }
 
 /*
